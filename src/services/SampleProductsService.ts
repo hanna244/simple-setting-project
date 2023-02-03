ed0a94a1fc6166d3237fcfc5e7b7a6bcf39c2546
hanna244 <shhn0509@gmail.com>
2023-02-03 18:30:32 +0900

    세팅 파일 구조 정리

--- src/services/SampleProductsService.ts
+++
@@ -1,7 +0,0 @@
-import axios from '.'
-
-export default class SampleProductsService {
-  static async categories() {
-    return await axios.get('products/categories')
-  }
-}
--- src/services/SampleUserService.ts
+++
@@ -1,9 +0,0 @@
-import axios from '.'
-
-import * as userInfoType from '@/types/sampleUserProfileType'
-
-export default class SampleUserService {
-  static async profile(params: userInfoType.RequestDataType) {
-    return await axios.get(`/users/${params.id}`, { params })
-  }
-}
